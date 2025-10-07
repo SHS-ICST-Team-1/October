2af6c5d9592a5e32fadc1a09e904178f4a128c16
shinkensen <noobywinner587@gmail.com>
2025-10-06 22:25:56 -0500

    base laptop and text

--- sketch.js
+++ sketch.js
@@ -1,21 +1,29 @@
-let g_laptop=[20,20,
-    20,40,
-    40,40,
-    40,20];
-let g_color=[140,140,140];
+let g_laptop=[];
+let g_color=[50,50,50];
+let height=500
+let num=31;
 
-
-
-let rect1= (x,y,w,h)=>{
-    console.log("" + x + +y +',' +x +(y+h) +',' + (x+w) + (y+h) +',' + (x+w) +y );
-    rect(x,y,w,h);
+let rect1 = (x, y, w, h, color) => {
+    console.log("" + x + "," + y + ',' + x + "," + (y + h) + ',' + (x + w) + "," + (y + h) + ',' + (x + w) + "," + y);
+    fill("rgb(" + color + ")");
+    rect(x, y, w, h);
 }
 function setup() {
     createCanvas(1525, 708);
 }
 function draw() {
     background(34);
-    polygon(g_laptop,g_color);
+    textSize(50);
+    fill(255, 255, 255);
+    text("SHS Team #1\n      Setups", 625, height);
+    if (height>150&&frameCount%2==0){
+        height-=num;
+        num/=1.1;
+    }
+    rect1(580, 300, 360, 200, "50,50,50");
+    g_laptop = [580, 500, 940, 500, 1020, 600, 500, 600];
+
+    polygon(g_laptop, g_color);
 }
 const polygon= (arr,rgb) =>{
     fill(rgb[0],rgb[1],rgb[2]);
