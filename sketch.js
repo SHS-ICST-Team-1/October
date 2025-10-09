18d269ffaf57bd8f97b5bcf9ea93dc212a80aadd
shinkensen <noobywinner587@gmail.com>
2025-10-08 21:07:10 -0500

    stuff

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let g_laptop=[];
+let g_laptopBase=[];
 let g_color=[50,50,50];
 let height=500
 let num=31;
@@ -21,9 +21,17 @@ function draw() {
         num/=1.1;
     }
     rect1(580, 300, 360, 200, "50,50,50");
-    g_laptop = [580, 500, 940, 500, 1020, 600, 500, 600];
+    rect1(590, 315, 340, 170, "20,20,20");
 
-    polygon(g_laptop, g_color);
+    let x=720,y=350;
+    rect1(x, y, 40, 40, "0,120,212");
+    rect1(x+45, y +45, 40, 40, "0,120,212");
+    rect1(x, y +45, 40, 40, "0,120,212");
+    rect1(x+45, y, 40, 40, "0,120,212");
+    g_laptopBase = [580, 500, 940, 500, 1020, 600, 500, 600];
+    polygon([1020, 600, 990,607, 530, 607, 500, 600],[45,45,45])
+    polygon([580, 500, 940, 500, 1020, 600, 500, 600])
+    polygon(g_laptopBase, g_color);
 }
 const polygon= (arr,rgb) =>{
     fill(rgb[0],rgb[1],rgb[2]);
