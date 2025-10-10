ec3fc3a1f586f2de74ce882e3cbe40348a486628
siddharthn <correctious@gmail.com>
2025-10-09 20:06:11 -0500

    made macbook started work on apple leaf

--- sketch.js
+++ sketch.js
@@ -32,6 +32,53 @@ function draw() {
     polygon([1020, 600, 990,607, 530, 607, 500, 600],[45,45,45])
     
     polygon(g_laptopBase, g_color);
+
+
+    //siddharth laptop (best one)
+    let macx = 75;
+    let macy = 300;
+    let macw = 340;
+    let mach = 200;
+    fill(70);
+    rect(macx,macy,macw,mach); //overall
+    fill(20);
+    rect(macx+10,macy+10,macw-20,mach-20,5); //screen
+    fill(100);
+
+    beginShape(); //base start
+    fill(70)
+    stroke(0,0,0);
+    vertex(macx, macy + mach); //tpl
+    vertex(macx + macw, macy+mach);//tpr
+    vertex(macx + macw+60, macy + mach + 100);//btr
+    vertex(macx-60, macy + mach + 100);//btl
+    endShape(CLOSE); //base end
+
+    //bottom of base
+    beginShape();
+    fill(70);
+    stroke(0,0,0);
+    vertex(macx-60, macy + mach + 100);//tpl
+    vertex(macx + macw+60, macy + mach + 100);//tpr
+    vertex(macx + macw+40, macy+mach+107); //btr
+    vertex(macx-40, macy+mach+107); //btl
+    endShape(CLOSE);
+
+    //apple logo (so cool)
+    let logox = macx + macw/2;
+    let logoy = macy + mach/2 - 10;
+
+    push();//begin drawing grp
+    translate(logox,logoy);
+    scale(1);
+
+    fill(255,255,255);
+    noStroke();
+
+    beginShape();
+    // 0,-40 ref point
+    endShape(CLOSE);
+    pop();
 }
 const polygon= (arr,rgb) =>{
     fill(rgb[0],rgb[1],rgb[2]);
